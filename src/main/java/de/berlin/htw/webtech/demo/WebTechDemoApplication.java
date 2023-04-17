12ec516e0d160f1b082a975465402194c33abe20
ManduulB <130990639+ManduulB@users.noreply.github.com>
2023-04-17 17:32:00 +0200

    firstcommit1

---
+++ src/main/java/de/berlin/htw/webtech/demo/WebTechDemoApplication.java
@@ -0,0 +1,13 @@
+package de.berlin.htw.webtech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTechDemoApplication.class, args);
+	}
+
+}
