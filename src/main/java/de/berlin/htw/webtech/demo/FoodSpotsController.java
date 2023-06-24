0c63eda32cf0ae393acc553a71111ddb3402a240
ManduulB <130990639+ManduulB@users.noreply.github.com>
2023-06-25 00:09:29 +0200

    controller geändert

--- src/main/java/de/berlin/htw/webtech/demo/FoodSpotsController.java
+++ src/main/java/de/berlin/htw/webtech/demo/FoodSpotsController.java
@@ -1,40 +1,103 @@
-package de.berlin.htw.webtech.demo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import java.util.Objects;
 
-import java.util.List;
+@Entity
+public class FoodSpots {
 
-@RestController
-public class FoodSpotsController {
-
-    @Autowired
-    FoodSpotService service;
-
-
-        Logger logger = LoggerFactory.getLogger(FoodSpotsController.class);
-
-        @PostMapping("/FoodSpotsList")
-        public FoodSpots createFoodSpot(@RequestBody FoodSpots foodSpots){
-            return service.save(foodSpots);
-        }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String category;
+    private String address;
+    private int rating;
+    private String website;
 
 
+    public FoodSpots(){}
 
-        @GetMapping("/FoodSpotsList/{id}")
-        public FoodSpots getFoodSpots(@PathVariable String id) {
-            logger.info("GET request on route FoodSpots with {}", id);
-            Long FoodSpotId = Long.parseLong(id);
-            return service.get(FoodSpotId);
-        }
+    public FoodSpots(String name, String category, String address, int rating, String website){
+        this.name = name;
+        this.category=category;
+        this.address=address;
+        this.rating=rating;
+        this.website=website;
+    }
 
-        @GetMapping("/FoodSpotsList")
-        public List<FoodSpots> getAllFoodSpotsList() {
-            return service.getAll();
-        }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FoodSpots foodSpots)) return false;
+        return getRating() == foodSpots.getRating() && Objects.equals(getId(), foodSpots.getId()) && Objects.equals(getName(), foodSpots.getName()) && Objects.equals(getCategory(), foodSpots.getCategory()) && Objects.equals(getAddress(), foodSpots.getAddress()) && Objects.equals(getWebsite(), foodSpots.getWebsite());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getCategory(), getAddress(), getRating(), getWebsite());
+    }
+
+
+    @Override
+    public String toString() {
+        return "FoodSpots{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", address='" + address + '\'' +
+                ", rating=" + rating + '\'' +
+                ", website=" + website +
+                '}';
+    }
 }
-
-
-
