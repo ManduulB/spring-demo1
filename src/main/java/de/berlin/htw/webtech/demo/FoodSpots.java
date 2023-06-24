555feb87a953b0dbfaa4dc00dbfa8312a3ff162b
ManduulB <130990639+ManduulB@users.noreply.github.com>
2023-06-25 00:04:27 +0200

    git ignore angepasst

--- src/main/java/de/berlin/htw/webtech/demo/FoodSpots.java
+++ src/main/java/de/berlin/htw/webtech/demo/FoodSpots.java
@@ -1,5 +1,3 @@
-package de.berlin.htw.webtech.demo;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,15 +17,14 @@ public class FoodSpots {
     private int rating;
     private String website;
 
+    public FoodSpots() {}
 
-    public FoodSpots(){}
-
-    public FoodSpots(String name, String category, String address, int rating, String website){
+    public FoodSpots(String name, String category, String address, int rating, String website) {
         this.name = name;
-        this.category=category;
-        this.address=address;
-        this.rating=rating;
-        this.website=website;
+        this.category = category;
+        this.address = address;
+        this.rating = rating;
+        this.website = website;
     }
 
     public Long getId() {
@@ -81,7 +78,8 @@ public class FoodSpots {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof FoodSpots foodSpots)) return false;
+        if (!(o instanceof FoodSpots)) return false;
+        FoodSpots foodSpots = (FoodSpots) o;
         return getRating() == foodSpots.getRating() && Objects.equals(getId(), foodSpots.getId()) && Objects.equals(getName(), foodSpots.getName()) && Objects.equals(getCategory(), foodSpots.getCategory()) && Objects.equals(getAddress(), foodSpots.getAddress()) && Objects.equals(getWebsite(), foodSpots.getWebsite());
     }
 
@@ -90,7 +88,6 @@ public class FoodSpots {
         return Objects.hash(getId(), getName(), getCategory(), getAddress(), getRating(), getWebsite());
     }
 
-
     @Override
     public String toString() {
         return "FoodSpots{" +
@@ -98,9 +95,8 @@ public class FoodSpots {
                 ", name='" + name + '\'' +
                 ", category='" + category + '\'' +
                 ", address='" + address + '\'' +
-                ", rating=" + rating + '\'' +
-                ", website=" + website +
+                ", rating=" + rating +
+                ", website='" + website + '\'' +
                 '}';
     }
 }
-
